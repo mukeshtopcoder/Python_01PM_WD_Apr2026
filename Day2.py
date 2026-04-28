4a4769aa13a2fac7c0cf9d3c80906c43fba37c7f
Mukesh <mukeshtopcoder@gmail.com>
2026-04-28 14:00:11 +0530

    UPdated

---
+++ Day2.py
@@ -0,0 +1,100 @@
+# Single Line Comment
+"""
+Multi line comments
+
+Built-in Functions
+    print , input , int , float , type
+PRINT
+
+print("Hello India")
+print("Mohan Singh")
+
+a = 100
+Hello = 9999
+print("a")
+print(a)
+print("Hello")
+print(Hello)
+
+
+print("Hello ")
+print("India")
+
+# \n => new line
+print("Hello \nIndia")
+
+
+a = 100
+print("Hello ",a,"Bye")
+
+
+a = 12
+b = 13
+c = a+b
+print("Addition :",c)
+
+
+
+INPUT
+input() => it is use to ask value from the user
+input() returns the text/string value
+
+a = input("Enter A Number : ")
+b = input("Enter B Number : ")
+c = a+b
+print("Addition :",c)
+
+
+INT
+int()  => it is use to convert a value into numeric
+
+a = int( input("Enter A Number : ") )
+b = int( input("Enter B Number : ") )
+c = a+b
+print("Addition :",c)
+
+int can not handle float/decimal/point value
+
+FLOAT
+
+a = float( input("Enter A Number : ") )
+b = float( input("Enter B Number : ") )
+c = a+b
+print("Addition :",c)
+
+# float can also support point values
+
+
+# WAP to calculate the gross salary of an employee where
+HRA is 20% and DA is 30% of his basic salary.
+HINT:- Gross_Salary = Basic_Salary + HRA + DA
+
+bs = float( input("Enter Basic Salary : ") )
+hra = bs*0.20
+da  = bs*0.30
+gs  = bs+hra+da
+print("Gross Salary :",gs)
+
+
+TYPE
+a = float( input("Enter a Number : ") )
+print( a )
+print( type(a) )
+
+a = 10
+print( type(a) )
+a = 17.3
+print( type(a) )
+a = 'aman'
+print( type(a) )
+a = True
+print( type(a) )
+
+# WAP to calculate the area of circle
+# HINT:- area = pie*raidus*radius
+
+"""
+radius = float(input("Enter Radius : "))
+area = 3.14*radius*radius
+print("Area :",area)
+
