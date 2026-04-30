b6929ac3d9fc12a44caab7f6540e2e55232868e0
Mukesh <mukeshtopcoder@gmail.com>
2026-04-30 18:53:07 +0530

    Updated

---
+++ Day4.py
@@ -0,0 +1,59 @@
+"""
+6- Logical Operator
+    and  , or  , not
+
+a = 7
+b = 5
+print( a>b and b>a )
+print( a>b or b>a )
+print( not a>b )
+
+
+print( int(True) )
+print( int(False) )
+
+
+print( bool(1) )
+print( bool(0) )
+print( bool(False) )
+print( bool() )
+print( bool(235) )
+print( bool(-5634) )
+print( bool('aman') )
+print( bool(345.5467) )
+
+
+a = 7
+b = 5
+print( a and b )
+print( b and a )
+
+
+a = 7
+b = 5
+print( a or b )
+print( b or a )
+
+
+a = 46
+print( not a )
+print( not 0 )
+
+
+7- Bitwise Operator
+    and ->  &
+    or  ->  |
+    not ->  ~
+
+a = 21
+b = 56
+print( a & b )  # 16
+print( a | b )  # 61
+
+
+a = 23
+b = 44
+print( a & b )  # 4
+print( a | b )  # 63
+
+"""
